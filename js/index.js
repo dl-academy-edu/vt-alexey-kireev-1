6bff488e2e07ac07b11200d4347ff6f6acb22db7
Dukalisss <dukalispr@gmail,com>
2020-12-03 02:37:48 +0400

    Сдача проекта

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-var html = document.querySelector("html");
-var popupbuttonclose = document.querySelector("#popup-close-button");
-var popupbuttonopen = document.querySelector("#popup-open-button");
-
-popupbuttonopen.addEventListener("click", function() {
-html.classList.add("popup-open");
-});
-
-popupbuttonclose.addEventListener("click", function() {
-    html.classList.remove("popup-open");
-    });
-    
-    
+var html=document.querySelector("html"),popupbuttonclose=document.querySelector("#popup-close-button"),popupbuttonopen=document.querySelector("#popup-open-button");popupbuttonopen.addEventListener("click",function(){html.classList.add("popup-open")}),popupbuttonclose.addEventListener("click",function(){html.classList.remove("popup-open")});
